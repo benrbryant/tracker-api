4dfaeb33d0307e056ab1eaf50a411d392af9adca
Ben Bryant <brbryant2639@gmail.com>
2023-06-19 14:48:46 -0500

    updated isLoggedIn to use req.isAuthenticated()

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,15 +1,9 @@
-import { AUTH_SECRET, AUTH_CLIENTID, AUTH_ISSUER, BASE_URL } from "../config";
-import { auth } from "express-openid-connect";
+import createHttpError from "http-errors";
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: AUTH_SECRET,
-  baseURL: BASE_URL,
-  clientID: AUTH_CLIENTID,
-  issuerBaseURL: AUTH_ISSUER,
-};
-
-const authMiddleware = auth(config);
-
-export default authMiddleware;
+export default function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    next(createHttpError(401));
+  }
+}
