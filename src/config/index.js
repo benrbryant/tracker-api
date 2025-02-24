4cbd39c96fa0d57221cc7188134e11fc225de30a
Ben Bryant <brbryant@uabmc.edu>
2025-02-24 09:43:56 -0600

    updated token expiration value

--- src/config/index.js
+++ src/config/index.js
@@ -7,5 +7,5 @@ export const MONGODB_URL = process.env.MONGODB_URL;
 export const MONGODB_DATABASE = process.env.MONGODB_DATABASE;
 export const REQUEST_RATE_TIME_LIMIT = 60000; // milliseconds
 export const MAX_REQUEST_COUNT_LIMIT = 20;
-export const SESSION_EXPIRATION = 1000 * 60 * 60 * 24 * 7; // 1 week
+export const SESSION_EXPIRATION = 60 * 60 * 24 * 7; // 1 week in seconds
 export const AUTH_SECRET = process.env.AUTH_SECRET;
