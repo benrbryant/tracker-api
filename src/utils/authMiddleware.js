589d5e8f166f6f1cfbc7ffa778e8cc6b5231ce4e
Ben Bryant <brbryant2639@gmail.com>
2025-02-24 23:23:43 -0600

    added local and jwt strategies

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,6 +1,11 @@
 import createHttpError from "http-errors";
 import jwt from "jsonwebtoken";
-import { AUTH_SECRET } from "../config";
+import passport from "passport";
+import bcrypt from "bcrypt";
+import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { ACCESS_TOKEN_SECRET } from "../config";
+import User from "../db/models/user";
 
 export function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
@@ -19,16 +24,58 @@ export function authenticate(req, res, next) {
   }
 
   if (token) {
-    jwt.verify(token, AUTH_SECRET, (err, username) => {
-      if (err) {
-        next(createHttpError(403));
-      }
-
-      req.user = username;
+    try {
+      let user = jwt.verify(token, ACCESS_TOKEN_SECRET);
+      req.user = user;
 
       next();
-    });
+    } catch (error) {
+      console.error(error);
+      next(createHttpError(403));
+    }
   } else {
     next(createHttpError(401));
   }
+}
+
+export const authenticateJwt = passport.authenticate("jwt", { session: false });
+
+export const localStrategy = new LocalStrategy(async function (
+  username,
+  password,
+  done
+) {
+  let existingUser = await User.findOne({ username });
+
+  if (!existingUser) {
+    return done(null, false);
+  }
+
+  const validPassword = await bcrypt.compare(password, existingUser.password);
+
+  if (!validPassword) {
+    return done(null, false, { message: "Invalid credentials" });
+  }
+
+  return done(null, existingUser);
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.ACCESS_TOKEN_SECRET,
 };
+
+export const jwtStrategy = new JwtStrategy(
+  jwtOptions,
+  async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.userId);
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
+    } catch (error) {
+      return done(error, false);
+    }
+  }
+);
