d2791b1b1a37c8ad45eee34a47e8358281d09594
Ben Bryant <brbryant@uabmc.edu>
2025-02-25 08:41:28 -0600

    updated return user payload fields

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,20 @@
+import createHttpError from "http-errors";
+import User from "../db/models/user";
 import asyncHandler from "../utils/asyncHandler";
 
-export const getCurrentUser = asyncHandler((req, res, next) => {
+export const getCurrentUser = asyncHandler(async (req, res, next) => {
+  let currentUser = await User.findById(req.user.userId, {
+    firstName: 1,
+    lastName: 1,
+    username: 1,
+    email: 1,
+  });
+
+  if (!currentUser) {
+    return next(createHttpError(404));
+  }
+
   res.json({
-    data: req.user,
+    data: currentUser,
   });
 });
